f675e78a3ab17d0edde1a2c83699f0d3c07b9418
Scott Simpson <Scott@simply-simpy.com>
2021-04-28 13:28:21 -0700

    add to github

---
+++ readme.md
@@ -0,0 +1,9 @@
+Web components tab example utilizing https://stenciljs.com/
+
+Run:
+```bash
+npm install
+npm start
+```
+Expected rendering will be 4 tabs with hover state.
+![](.readme_images/8e0fd752.png)
---
+++ src/components/flight-search-tab/flight-search-tab.tsx
@@ -0,0 +1,27 @@
+import { Component, h, Prop } from '@stencil/core';
+
+@Component({
+  tag: 'flight-search-tab',
+  styleUrl: 'flight-search-tab.css',
+  shadow: true,
+})
+export class FlightSearchTab {
+
+  @Prop() tabname: string;
+  @Prop() tabswitchname: string;
+  @Prop() text: string;
+
+  private getText(text): string {
+    this.text = text;
+    return this.text;
+  }
+
+  render() {
+    return <div>
+      <a class="tab-switch" href={this.getText(this.tabswitchname)}>
+        {this.getText(this.tabname)}
+      </a>
+    </div>;
+  }
+
+}
